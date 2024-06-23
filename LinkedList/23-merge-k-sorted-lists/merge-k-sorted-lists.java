11f6e085b9327e33b804f700396af0f232a65f35
Logesh Suresh <64845710+logeshsuresh@users.noreply.github.com>
2024-06-23 16:11:47 +0530

    Time: 4 ms (74.48%) | Memory: 44.3 MB (58.35%) - LeetSync

--- LinkedList/23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ LinkedList/23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -10,41 +10,41 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists.length == 0) { return null; }
-        if (lists.length == 1) { return lists[0]; }
-        
-        // Initialize the result with the first list
-        ListNode res = lists[0];
-        
-        // Merge the rest of the lists one by one
-        for (int i = 1; i < lists.length; i++) {
-            res = mergeTwoLists(res, lists[i]);
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+
+        ListNode dummy = new ListNode(-1);
+        ListNode dummyPtr = dummy;
+
+        for (ListNode nodeHead : lists) {
+            if (nodeHead != null) { pq.offer(new Pair(nodeHead.val, nodeHead)); }
         }
-        
-        return res;
+
+        while (!pq.isEmpty()) {
+            Pair p = pq.poll();
+            ListNode curNode = p.node;
+            dummyPtr.next = curNode;
+            curNode = curNode.next;
+            dummyPtr = dummyPtr.next;
+            dummyPtr.next = null;
+            if (curNode != null) {
+                pq.offer(new Pair(curNode.val, curNode));
+            }
+        }
+
+        return dummy.next;
+    }
+}
+
+class Pair implements Comparable<Pair> {
+    int value;
+    ListNode node;
+
+    public Pair(int value, ListNode node) {
+        this.value = value;
+        this.node = node;
     }
 
-    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode dummyHead = new ListNode(-1);
-        ListNode dummy = dummyHead;
-        while (l1 != null && l2 != null) {
-            if (l1.val <= l2.val) {
-                dummy.next = l1;
-                l1 = l1.next;
-            } else {
-                dummy.next = l2;
-                l2 = l2.next;
-            }
-            dummy = dummy.next;
-        }
-
-        if (l1 != null) {
-            dummy.next = l1;
-        }
-        if (l2 != null) {
-            dummy.next = l2;
-        }
-
-        return dummyHead.next;
+    public int compareTo(Pair other) {
+        return Integer.compare(this.value, other.value);
     }
 }
