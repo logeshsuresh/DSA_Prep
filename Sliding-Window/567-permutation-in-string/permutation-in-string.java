c2716c674c733d21dae9710beb0a02ce3851f74c
Logesh Suresh <64845710+logeshsuresh@users.noreply.github.com>
2024-06-29 22:23:35 +0530

    Time: 18 ms (41.99%) | Memory: 42.9 MB (43.19%) - LeetSync

---
+++ Sliding-Window/567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,51 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int n = s2.length();
+
+        Map<Character, Integer> charMap = new HashMap<>();
+
+        for (char ch : s1.toCharArray()) {
+            charMap.put(ch, charMap.getOrDefault(ch, 0) + 1);
+        }
+
+        int distinctCnt = charMap.size();
+
+        int windowSize = s1.length();
+
+        int start = 0, end = 0;
+
+        while (end < n) {
+            char ch = s2.charAt(end);
+            while (end - start + 1 > windowSize && start <= end) {
+                char charAtStart = s2.charAt(start);
+                if (charMap.containsKey(charAtStart)) {
+                    int cnt = charMap.get(charAtStart);
+                    if (cnt == 0) {
+                        distinctCnt++;
+                    }
+                    charMap.put(charAtStart, cnt + 1);
+                }
+                start++;
+            }
+
+            if (charMap.containsKey(ch)) {
+                int cnt = charMap.get(ch);
+                if (cnt == 1) {
+                    distinctCnt--;
+                }
+                charMap.put(ch, cnt - 1);
+            }
+
+            if (end - start + 1 == windowSize) {
+                if (distinctCnt == 0) { 
+                    System.out.println(s2.substring(start, end + 1));
+                    return true; 
+                }
+            }
+
+            end++;
+        }
+
+        return false;
+    }
+}
